9fed77a96b56f46381e86e417de911fee3f2fa93
owtf086 <owtf086@gmail.com>
2024-10-03 11:46:56 +0300

    init full

---
+++ projects/ui-kit/src/lib/components/button/button.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core';
+import { NgClass } from '@angular/common';
+
+@Component({
+  selector: 'ui-button',
+  standalone: true,
+  imports: [NgClass],
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss'],
+})
+export class ButtonComponent {
+  @Input() label: string = 'Click me';
+  @Input() type: 'primary' | 'secondary' = 'primary';
+  @Input() size: 'small' | 'medium' | 'large' = 'medium';
+
+  get buttonClass() {
+    return {
+      'btn-primary': this.type === 'primary',
+      'btn-secondary': this.type === 'secondary',
+      'btn-small': this.size === 'small',
+      'btn-medium': this.size === 'medium',
+      'btn-large': this.size === 'large',
+    };
+  }
+}
---
+++ projects/ui-kit/src/lib/components/button/button.stories.ts
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { ButtonComponent } from './button.component';
+
+const meta: Meta<ButtonComponent> = {
+  title: 'UI/Button',
+  component: ButtonComponent,
+  tags: ['autodocs'],
+  argTypes: {
+    type: {
+      control: 'select',
+      options: ['primary', 'secondary'],
+    },
+    label: {
+      control: 'text',
+    },
+    size: {
+      control: 'select',
+      options: ['small', 'medium', 'large'],
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<ButtonComponent>;
+
+export const Primary: Story = {
+  args: {
+    type: 'primary',
+    label: 'Primary Button',
+    size: 'medium',
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    type: 'secondary',
+    label: 'Secondary Button',
+    size: 'medium',
+  },
+};
